e3ea5b39d10adbb7fb8d9a2655e2d040a8d678f2
Ocnaru Mihai <masterpaper01@gmail.com>
2023-03-01 11:21:32 +0200

    Create heap.h

---
+++ headers/heap.h
@@ -0,0 +1,14 @@
+#ifndef HEAP_H_INCLUDED
+#define HEAP_H_INCLUDED
+
+
+
+#endif // HEAP_H_INCLUDED
+
+
+#define MAX_CHARS 1000
+typedef struct data{
+    char word[MAX_CHARS];
+    float probability;
+}data;
+
